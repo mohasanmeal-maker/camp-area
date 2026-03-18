c1a1db0e2524a2c969643515b09eafb1fc6da126
Mohasan Uddin Tutul <mohasan.meal@gmail.com>
2026-03-18 16:38:26 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,20 @@
-const CACHE_NAME = 'fivdb-map-v1';
+const CACHE_NAME = 'fivdb-map-v2';
 
-const urlsToCache = [
-  '/',
-  '/index.html',
-
-  './resources/ol.js',
-  './resources/ol.css',
-  './resources/qgis2web.js',
-  './resources/ol-layerswitcher.js',
-  './resources/photon-geocoder-autocomplete.min.js',
-
-  './layers/layers.js',
-  './layers/FacilityInfo_4.js',
-
-  './styles/FacilityInfo_4_style.js',
-
-  './FIVDB.jpg'
-];
-
-// Install
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Caching files...');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html'
+      ]);
+    })
   );
 });
 
-// Fetch
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    fetch(event.request).catch(function() {
+      return caches.match('./index.html');
+    })
   );
 });
